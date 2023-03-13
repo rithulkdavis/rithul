98cc471281c4bb9cef7476f4f424f6f2df79351c
muhsina-km <muhsim421@gmail.com>
2023-03-13 02:42:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+//var x=20;
+//if(x>20){
+   // console.log("good");
+
+//}
+//else
+//console.log("bad");
+//for(var i=1;i<=5;i++)
+//{
+    //console.log(i)
+
+//}
+var fruits=["mango","orange","apple","bannana"];
+//for (let i=0;i<fruits.length;i++)
+//{
+// console.log(fruits[i])
+//}
+//let p=0;
+//while(p<fruits.length){
+   // /console.log(fruits[p]);
+    //p++;
+//}
+let i=0;
+do{
+    console.log(fruits[i]);
+    i++
+
+}while(i<fruits.length)
